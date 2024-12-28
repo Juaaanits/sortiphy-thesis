d943376ec35a1b557dac45c6783f246b663634fc
NiTzuA <11v35faustin@gmail.com>
2024-12-28 12:37:46 +0800

    Added temporary button in LoginScreen
    This commit adds a temporary functionality in the "log in" button of the login screen. Its function is to switch from the LoginScreen activity to the MainActivity Activity.

--- app/app/src/main/java/com/reviewers/sortiphy/LoginScreen.java
+++ app/app/src/main/java/com/reviewers/sortiphy/LoginScreen.java
@@ -1,6 +1,9 @@
 package com.reviewers.sortiphy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,18 @@ public class LoginScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        configureNextButton();
+    }
+    private void configureNextButton() {
+        Button loginButton = (Button) findViewById(R.id.login_confirm_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginScreen.this, MainActivity.class));
+                finish();
+            }
         });
     }
 }
