ad6aa58798977a710d17304776319ffc3799f1f6
NiTzuA <11v35faustin@gmail.com>
2025-01-04 18:04:30 +0800

    Added a ViewPager2 in Dashboard
    This commit includes the implementation of a ViewPager2 in the dashboard fragment of the application. Note that the implementation is still incomplete and needs a page number implementation as well as an implementation for multiple bin statuses in a single page.

--- app/app/src/main/java/com/reviewers/sortiphy/DashboardFragment.java
+++ app/app/src/main/java/com/reviewers/sortiphy/DashboardFragment.java
@@ -6,12 +6,46 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import org.jetbrains.annotations.NotNull;
 
 public class DashboardFragment extends Fragment {
-
+/*
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dashboard,container,false);
     }
+*/
 
+    private static final int NUM_PAGES = 3;
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_dashboard,container,false);
+        viewPager = rootView.findViewById(R.id.bin_dashboard_levels_pager);
+        pagerAdapter = new DashboardFragmentPagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+
+        return rootView;
+    }
+
+    private class DashboardFragmentPagerAdapter extends FragmentStateAdapter {
+        public DashboardFragmentPagerAdapter(@NotNull Fragment fragment) {
+            super(fragment);
+        }
+        @NotNull
+        @Override
+        public Fragment createFragment(int position) {
+            return new DashboardPageViewFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
 }
