b3891e58efcbc6589d46b7cb6e7a953b042a9706
NiTzuA <11v35faustin@gmail.com>
2025-01-19 16:22:27 +0800

    Started Statistics Fragment
    This commit includes the creation of the fragment for the statistics. It currently includes a tab layout and a viewpager for all the information. Take note this is only a skeleton and has no functionality. Alongside this, the charts have not been added yet, as I am still learning how to make them.

--- app/app/src/main/java/com/reviewers/sortiphy/StatisticsFragment.java
+++ app/app/src/main/java/com/reviewers/sortiphy/StatisticsFragment.java
@@ -6,12 +6,59 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.jetbrains.annotations.NotNull;
 
 public class StatisticsFragment extends Fragment {
+/*
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard,container,false);
+    }
+*/
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_statistics,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_statistics,container,false);
+        viewPager = rootView.findViewById(R.id.statistics_viewpager);
+        pagerAdapter = new FragmentPagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout = rootView.findViewById(R.id.statistics_tabLayout);
+
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(TabLayout.Tab tab, int position) {
+                // Configure the tab, e.g., set text or icon
+                tab.setText("Tab " + (position + 1));
+            }
+        }).attach();
+
+        return rootView;
     }
 
+    private class FragmentPagerAdapter extends FragmentStateAdapter {
+        public FragmentPagerAdapter(@NotNull Fragment fragment) {
+            super(fragment);
+        }
+        @NotNull
+        @Override
+        public Fragment createFragment(int position) {
+            return new StatisticsPageViewFragment();
+        } // This part is responsible for deciding which class will show what fragment
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
 }
---
+++ app/app/src/main/java/com/reviewers/sortiphy/StatisticsPageViewFragment.java
@@ -0,0 +1,15 @@
+package com.reviewers.sortiphy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class StatisticsPageViewFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(R.layout.viewpager_statistics_fragment, container, false);
+    }
+}
