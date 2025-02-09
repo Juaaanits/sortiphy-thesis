be31c75bccc84be54a0534e7a27c9fa1a5687775
NiTzuA <11v35faustin@gmail.com>
2025-02-09 15:11:04 +0800

    Implemented ViewPager Firebase Linking
    This commit links the viewpager on the dashboard to the Firestore Cloud Database.

--- app/app/src/main/java/com/reviewers/sortiphy/DashboardFragment.java
+++ app/app/src/main/java/com/reviewers/sortiphy/DashboardFragment.java
@@ -1,9 +1,11 @@
 package com.reviewers.sortiphy;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -11,6 +13,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -21,15 +24,32 @@ public class DashboardFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_dashboard,container,false);
     }
 */
-
+    private FirebaseFirestore db;
     private static final int NUM_PAGES = 3;
     private ViewPager2 viewPager;
     private FragmentStateAdapter pagerAdapter;
     private TabLayout tabLayout;
+    String userId = "SqkFypben8amVK4o4RED"; //replace this when authentication system is complete!
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
         View rootView = inflater.inflate(R.layout.fragment_dashboard,container,false);
+        TextView usernameDisplay = rootView.findViewById(R.id.greetings_text);
+
+        db.collection("users").document(userId)
+                .addSnapshotListener((documentSnapshot, error) -> {
+                    if (error != null) {
+                        Log.e("Firestore", "Listen failed.", error);
+                        return;
+                    }
+                    if (documentSnapshot.exists()) {
+                        String username = documentSnapshot.getString("username");
+                        usernameDisplay.setText("Welcome, " + username + "!");
+                    }
+                });
+
+
         viewPager = rootView.findViewById(R.id.bin_dashboard_levels_pager);
         pagerAdapter = new DashboardFragmentPagerAdapter(this);
         viewPager.setAdapter(pagerAdapter);
@@ -53,7 +73,7 @@ public class DashboardFragment extends Fragment {
         @NotNull
         @Override
         public Fragment createFragment(int position) {
-            return new DashboardPageViewFragment();
+            return DashboardPageViewFragment.newInstance(position, position + 2);
         }
 
         @Override
--- app/app/src/main/java/com/reviewers/sortiphy/DashboardPageViewFragment.java
+++ app/app/src/main/java/com/reviewers/sortiphy/DashboardPageViewFragment.java
@@ -1,17 +1,136 @@
 package com.reviewers.sortiphy;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class DashboardPageViewFragment extends Fragment {
+
+    private FirebaseFirestore db;
+    private String classOne;
+    private String classTwo;
+    private int inputOne;
+    private int inputTwo;
+
+    public static DashboardPageViewFragment newInstance(int inputOne, int inputTwo) {
+        DashboardPageViewFragment fragment = new DashboardPageViewFragment();
+        Bundle args = new Bundle();
+        args.putInt("INPUT_ONE", inputOne);
+        args.putInt("INPUT_TWO", inputTwo);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            inputOne = getArguments().getInt("INPUT_ONE", 0);
+            inputTwo = getArguments().getInt("INPUT_TWO", 0);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return (ViewGroup) inflater.inflate(R.layout.viewpager_item, container, false);
+        db = FirebaseFirestore.getInstance();
+        switch (inputOne) {
+            case 0:
+                classOne = "trashClassificationOne";
+                break;
+            case 1:
+                classOne = "trashClassificationTwo";
+                break;
+            case 2:
+                classOne = "trashClassificationThree";
+                break;
+            case 3:
+                classOne = "trashClassificationFour";
+                break;
+            case 4:
+                classOne = "trashClassificationFive";
+                break;
+            default:
+                classOne = "trashClassificationOne";
+        }
+
+        switch (inputTwo) {
+            case 1:
+                classTwo = "trashClassificationTwo";
+                break;
+            case 2:
+                classTwo = "trashClassificationThree";
+                break;
+            case 3:
+                classTwo = "trashClassificationFour";
+                break;
+            case 4:
+                classTwo = "trashClassificationFive";
+                break;
+            case 5:
+                classTwo = "trashClassificationFive";
+                break;
+            default:
+                classTwo = "trashClassificationOne";
+        }
+        View rootView = inflater.inflate(R.layout.viewpager_item, container, false);
+        TextView fillLevelOne = rootView.findViewById(R.id.text_num_percent_one);
+        TextView fillLevelTwo = rootView.findViewById(R.id.text_num_percent_two);
+        TextView binLevelOne = rootView.findViewById(R.id.bin_desc_one);
+        TextView binLevelTwo = rootView.findViewById(R.id.bin_desc_two);
+        TextView bottomText = rootView.findViewById(R.id.text_percent_one);
+        TextView bottomTextTwo = rootView.findViewById(R.id.text_percent_two);
+
+        db.collection("binData").document(classOne)
+            .addSnapshotListener((documentSnapshot, error) -> {
+                    if (error != null) {
+                        Log.e("Firestore", "Listen failed.", error);
+                        return;
+                    }
+                    if (documentSnapshot.exists()) {
+                        long percentageValue = documentSnapshot.getLong("fillLevel");
+                        String binDesc = documentSnapshot.getString("className");
+                        if (percentageValue > 90) {
+                            fillLevelOne.setText("Replace");
+                            bottomText.setText("Bag Now");
+                        } else {
+                            fillLevelOne.setText(String.valueOf(percentageValue));
+                            bottomTextTwo.setText("Percent");
+                        }
+                        binLevelOne.setText(binDesc);
+                    }
+                 });
+
+        db.collection("binData").document(classTwo)
+                .addSnapshotListener((documentSnapshot, error) -> {
+                    if (error != null) {
+                        Log.e("Firestore", "Listen failed.", error);
+                        return;
+                    }
+                    if (documentSnapshot.exists()) {
+                        long percentageValue = documentSnapshot.getLong("fillLevel");
+                        String binDesc = documentSnapshot.getString("className");
+                        if (percentageValue > 90) {
+                            fillLevelTwo.setText("Replace");
+                            bottomTextTwo.setText("Bag Now");
+                        } else {
+                            fillLevelTwo.setText(String.valueOf(percentageValue));
+                            bottomTextTwo.setText("Percent");
+                        }
+                        binLevelTwo.setText(binDesc);
+                    }
+                });
+        return rootView;
     }
 }
-// hi!
